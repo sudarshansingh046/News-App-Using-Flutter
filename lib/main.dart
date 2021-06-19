a00c0b1b72cd45c3229c4521120cc35d359521d9
Amit89499 <amitkhandelwal89499@gmail.com>
2021-06-19 23:02:07 +0530

    file added

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutternews/views/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Khabar Brief',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: Home(),
+    );
+  }
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Khabar"),
+            Text(
+              "Brief",
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+        elevation: 0.0,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryName;
+  CategoryTile({this.imageUrl, this.categoryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Image.network(
+            imageUrl,
+            width: 120,
+            height: 60,
+          )
+        ],
+      ),
+    );
+  }
+}
