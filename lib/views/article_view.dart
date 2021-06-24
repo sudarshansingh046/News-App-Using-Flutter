2c3fda8fcb2b3311c68214de5d26d236df4cac2f
Amit89499 <amitkhandelwal89499@gmail.com>
2021-06-25 00:51:35 +0530

    Categories added

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -1,15 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
+  final String blogUrl;
+  ArticleView({required this.blogUrl});
   @override
   _ArticleViewState createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Khabar"),
+            Text(
+              "Brief",
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          Opacity(
+            opacity: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.save),
+            ),
+          )
+        ],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: ((WebViewController webViewController) {
+            _completer.complete(webViewController);
+          }),
+        ),
+      ),
     );
   }
 }
--- lib/views/category_news.dart
+++ lib/views/category_news.dart
@@ -1,15 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:flutternews/helper/news.dart';
+import 'package:flutternews/models/article_model.dart';
+import 'package:flutternews/views/article_view.dart';
 
+// ignore: must_be_immutable
 class CategoryNews extends StatefulWidget {
+  List<ArticleModel> news = [];
+  final String category;
+  CategoryNews({required this.category});
   @override
   _CategoryNewsState createState() => _CategoryNewsState();
 }
 
 class _CategoryNewsState extends State<CategoryNews> {
+  List<ArticleModel> articles = new List.empty();
+  bool _loading = true;
+  @override
+  void initState() {
+    super.initState();
+    //categories = getCategories();
+    getCategoryNews();
+  }
+
+  void getCategoryNews() async {
+    CategoryNewsClass newsClass = CategoryNewsClass();
+    await newsClass.getNews(widget.category);
+    articles = newsClass.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Khabar"),
+            Text(
+              "Brief",
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          Opacity(
+            opacity: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.save),
+            ),
+          )
+        ],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: _loading
+          ? Center(
+              child: Container(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 16),
+                      child: ListView.builder(
+                          itemCount: articles.length,
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return BlogTile(
+                              imageUrl: articles[index].urlToImage,
+                              title: articles[index].title,
+                              desc: articles[index].description,
+                              url: articles[index].url,
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc, url;
+  BlogTile(
+      {required this.imageUrl,
+      required this.title,
+      required this.desc,
+      required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ArticleView(
+                      blogUrl: url,
+                    )));
+      },
+      child: Container(
+          margin: EdgeInsets.only(bottom: 16),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Text(
+                desc,
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          )),
     );
   }
 }
